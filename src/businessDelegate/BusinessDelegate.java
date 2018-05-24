8d1552a62d9bf647ef3b987f9752ff37cd70d2f7
diegolamus <dalumes95@gmail.com>
2018-05-24 11:51:25 -0500

    Crear datos sql

--- src/businessDelegate/BusinessDelegate.java
+++ src/businessDelegate/BusinessDelegate.java
@@ -1,10 +1,10 @@
 package businessDelegate;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.Connection;
 import java.sql.Statement;
 import conexion.Conexion;
 
@@ -14,31 +14,21 @@ public class BusinessDelegate implements IBusinessDelegate {
 	public void crearBaseDeDatos(String usuario, String contrasenia) throws Exception {
 		Conexion.conectar(usuario, contrasenia);
 		Statement stm = Conexion.getConexion().createStatement();
+		stm.executeUpdate(leerSQL(""));	
+		stm.close();
 		Conexion.cerrarConexion();
 	}
 	
-	public boolean executeQuery(String query){
-        Statement sentencias = null;
-        try{ 
-            sentencias = Conexion.getConexion().createStatement(); 
-            sentencias.executeUpdate(query); 
-            sentencias.close(); 
-        }catch(Exception e){ 
-            return false; 
-        } 
-        return true;
-    }
-
-	public String leerSQL(java.io.File documento) {
+	public String leerSQL(String direccion) {
 		String sCadena = "";
 		String retorno = "";
-
-		if (!documento.exists()) {
+		File file = new File(direccion);
+		if (!file.exists()) {
 			return null;
 		}
 
 		try {
-			BufferedReader bf = new BufferedReader(new FileReader(documento));
+			BufferedReader bf = new BufferedReader(new FileReader(file));
 			while ((sCadena = bf.readLine()) != null) {
 				retorno += sCadena;
 			}
